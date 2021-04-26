15d5b79d242dfe33ceb29ec9296aadd5f1d34772
Ali <76096715+AliEzberbegov@users.noreply.github.com>
2021-04-26 18:30:39 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,12 +2,103 @@
 #include <string>
 #include "tstack.h"
 
+int sequence(char value) {
+    switch (value) {
+      case '(': return 0;
+          break;
+      case ')': return 1;
+          break;
+      case '+': return 2;
+          break;
+      case '-': return 2;
+          break;
+      case '*': return 3;
+          break;
+      case '/': return 3;
+          break;
+      default: return -1;
+    }
+}
+
 std::string infx2pstfx(std::string inf) {
-  // добавьте сюда нужный код
-  return std::string("");
+Tstack <char> MyStack;
+int prioritet;
+char arr;
+char begin = 0;
+
+std::string total;
+for (int i = 0; i < inf.length(); i++) {
+   arr = inf[i];
+   prioritet = sequence(arr);
+   if (prioritet > -1) {
+            if ((prioritet == 0 || prioritet > sequence(begin) || MyStack.isEmpty()) && arr != ')') {
+                if (MyStack.isEmpty())
+                    begin = arr;
+                    MyStack.push(arr);
+            }
+            else if (arr == ')') {
+                while (MyStack.get() != '(') {
+                    total.push_back(MyStack.get());
+                    total.push_back(' ');
+                    MyStack.pop();
+                }
+                MyStack.pop();
+                if (MyStack.isEmpty())
+                    begin = 0;
+            }
+            else {
+                while (!MyStack.isEmpty() && sequence(MyStack.get()) >= prioritet) {
+                    total.push_back(MyStack.get());
+                    total.push_back(' ');
+                    MyStack.pop();
+                }
+                if (MyStack.isEmpty())
+                    begin = inf[i];
+                    MyStack.push(inf[i]);
+            }
+        }
+        else {
+            total.push_back(arr);
+            total.push_back(' ');
+        }
+    }
+    while (!MyStack.isEmpty()) {
+        total.push_back(MyStack.get());
+        total.push_back(' ');
+        MyStack.pop();
+  }
+  total.erase(total.end() - 1, total.end());
+  return total;
 }
 
 int eval(std::string pst) {
-  // добавьте сюда нужный код
-  return 0;
+  TStack <int> MyStack2;
+  std::string total2;
+  int amount;
+  for (int i = 0; i < pst.length(); i++) {
+    if (pst[i] >= '0' && pst[i] <= '9') {
+        MyStack2.push(pst[i] - '0');
+        total2 = pst[i];
+    }
+    else if (pst[i] != ' ') {
+      int val2 = MyStack2.get();
+      MyStack2.pop();
+      int val1 = MyStack2.get();
+      MyStack2.pop();
+      if (pst[i] == '*') {
+        MyStack2.push(val1 * val2);
+      }
+      else if (pst[i] == '/') {
+        MyStack2.push(val1 / val2);
+      }
+      else if (pst[i] == '+') {
+        MyStack2.push(val1 + val2);
+      }
+      else {
+        MyStack2.push(val1 - val2);
+      }
+    }
+  }
+  amount = MyStack2.get();
+  return amount;
 }
